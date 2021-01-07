ed62646cbe1b90bd7c7d864e9d2e6a460b25ce5a
diana <diliang4783573@gmail.com>
2021-01-06 20:29:28 -0800

    Added Post CRUD methods and routes

--- bloggies_backend/src/models/post.ts
+++ bloggies_backend/src/models/post.ts
@@ -1,12 +1,83 @@
 
 class Post {
 
-  /** Get specific user from database */
+  /** Create a new post */
   static async create(title: string, description: string, body: string, userId: number) {
-    const res = await db.query(
-      `INSERT INTO posts ( title, description, body, author_id )
-        VALUES ($1, $2, $3, $4)`, 
-      [ userId ]);
-    return res.rows[0];
+    try { 
+      const res = await db.query(
+        `INSERT INTO posts ( title, description, body, author_id )
+          VALUES ($1, $2, $3, $4)`, 
+        [ title, description, body, userId ]);
+      return res.rows[0];
+    } catch (err) {
+      throw new ExpressError(`Err: ${err}`, 400);
+    }
+  }
+
+  /** Get all existing posts */
+  static async getAll() {
+    try { 
+      const res = await db.query(
+        `SELECT id, title, description, body, author_id, created_at, last_updated_at
+        FROM posts`);
+      return res.rows;
+    } catch (err) {
+      throw new ExpressError(`Err: ${err}`, 400);
+    }
+  }
+
+  /** Get all existing posts */
+  static async getPost(id: number) {
+    try { 
+      const res = await db.query(
+        `SELECT id, title, description, body, author_id, created_at, last_updated_at
+        FROM posts
+        WHERE id = $1`,
+        [ id ]);
+      return res.rows[0];
+    } catch (err) {
+      throw new ExpressError(`Err: ${err}`, 400);
+    }
+  }
+
+  /** Check if post belongs to the current user */
+  static async isAuthor(postId: number, currentUserId: number) {
+    const post = await this.getPost(postId);
+    return post.author_id === currentUserId;
+  }
+
+  /** Update an existing posts */
+  static async update(id: number, updateData: any) {
+    try { 
+      let query = "";
+      
+      for (let key in updateData ) {
+        query = query + ` ${key} = ${updateData[key]}, `;
+      }
+
+      await db.query(
+        `UPDATE posts
+        SET` + query + `last_updated_at = CURRENT_TIMESTAMP 
+        WHERE id = $1`,
+        [ id ]);
+      
+      return { message: "Successfully updated." };
+    } catch (err) {
+      throw new ExpressError(`Err: ${err}`, 400);
+    }
+  }
+
+  static async delete(id: number) {
+    await db.query(
+      `DELETE FROM posts
+      WHERE id = $1`,
+      [ id ]);
+    return { message: "Successfully deleted." };
+  }
+
+  /** Searches based on term given - term can be a string or post date */
+  static async search(term: any) {
+    // if term is a string.. search posts by title
+    // if term is a date.. search posts by created_at dates
   }
 }
--- bloggies_backend/src/routes/posts.ts
+++ bloggies_backend/src/routes/posts.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { ensureLoggedIn } from "../auth";
 import { IUserRequest } from "../interfaces";
 
@@ -12,10 +12,84 @@ router.post("/", ensureLoggedIn, async function (req: IUserRequest, res: Respons
   try {
     const user = req.user;
     const { title, description, body } = req.body;
-    const result = await Post.create(title, description, body, user.user_id);
-    return res.json(result);
+    const post = await Post.create(title, description, body, user.user_id);
+    return res.status(201).send({ post });
   } catch (err) {
     return next(err);
   }
 });
 
+/** GET /posts - get all posts. 
+ * Returns posts */
+router.get("/", async function (req: Request, res: Response, next: NextFunction) {
+  try {
+    const posts = await Post.getAll();
+    return res.json({ posts });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /posts/:id - get a specific post by post id. 
+ * Returns posts */
+router.get("/:id", async function (req: Request, res: Response, next: NextFunction) {
+  try {
+    const postId = req.params.id;
+    const post = await Post.getPost(parseInt(postId));
+    return res.json({ post });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** PATCH /posts/:id - updates a specific post by post id. 
+ * MUST LOGGED IN AS THE AUTHOR OF POST.
+ * Returns a 204 code */
+router.patch("/:id", ensureLoggedIn, async function(req: IUserRequest, res: Response, next: NextFunction) {
+  try {
+    const postId = parseInt(req.params.id);
+    const currentUser = req.user
+
+    if (await Post.isAuthor(postId, currentUser.user_id)) {
+      const updateData = req.body;
+      await Post.update(postId, updateData);
+      return res.status(204);
+    }
+
+    return res.status(401);
+  } catch (err) {
+    return next(err)
+  }
+});
+
+/** DELETE /posts/:id - updates a specific post by post id. 
+ * MUST LOGGED IN AS THE AUTHOR OF POST.
+ * Returns message */
+router.delete("/:id", ensureLoggedIn, async function(req: IUserRequest, res: Response, next: NextFunction) {
+  try {
+    const postId = parseInt(req.params.id);
+    const currentUser = req.user;
+
+    if (await Post.isAuthor(postId, currentUser.user_id)) {
+      const message = await Post.delete(postId);
+      return res.json(message)
+    }
+
+    return res.status(401);
+  } catch (err) {
+    return next(err)
+  }
+});
+
+/** SEARCH /posts/search?term=[term] - get all posts. 
+ * Returns posts */
+router.get("/search", async function (req: Request, res: Response, next: NextFunction) {
+  try {
+    const term = req.query.term;
+    // TODO: search method
+    const posts = await Post.search(term);
+    return res.json({ posts });
+  } catch (err) {
+    return next(err);
+  }
+});
