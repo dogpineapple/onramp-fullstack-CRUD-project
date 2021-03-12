1f559bca4abf0e8956f9e14a4c70e8c4b29348c9
Diana <dliang@blend.com>
2021-03-12 15:19:10 -0800

    Update DB_URI value and utilize env variables

--- bloggies_backend/src/db.ts
+++ bloggies_backend/src/db.ts
@@ -1,13 +1,15 @@
 /** Database setup */
-
 import { Client } from "pg";
+
+let { POSTGRES_USER, POSTGRES_PWD, POSTGRES_PORT, NODE_ENV } = process.env; 
 
 let DB_URI;
 
-if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///learning_circle_test";
+// connect to PSQL container using docker
+if (NODE_ENV === "test") {
+  DB_URI = `postgresql://${POSTGRES_USER}:${POSTGRES_PWD}@localhost:${POSTGRES_PORT}/learning_circle_test`;
 } else {
-  DB_URI = "postgresql:///learning_circle";
+  DB_URI = "postgresql://${POSTGRES_USER}:${POSTGRES_PWD}@localhost:${POSTGRES_PORT}/learning_circle";
 }
 
 let db = new Client({
