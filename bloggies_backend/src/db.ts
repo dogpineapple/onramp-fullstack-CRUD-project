eba5c1023eb3783d53424ef589f7746d7b0577d3
abquansah <aquansah@blend.com>
2021-03-12 16:36:57 -0800

    added environment variables to add credentials for PostGres database

--- bloggies_backend/src/db.ts
+++ bloggies_backend/src/db.ts
@@ -1,13 +1,14 @@
 /** Database setup */
 
 import { Client } from "pg";
+require('dotenv').config({ path: require('find-config')('.env') })
 
 let DB_URI;
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///learning_circle_test";
+  DB_URI = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:5432/learning_circle_test`;
 } else {
-  DB_URI = "postgresql:///learning_circle";
+  DB_URI = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:5432/learning_circle`;
 }
 
 let db = new Client({
