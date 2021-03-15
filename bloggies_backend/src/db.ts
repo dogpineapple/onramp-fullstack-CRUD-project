3dd152d7c97659d1326f0a8586a135ca40419b9e
Megan McDevitt <mmcdevitt@blend.com>
2021-03-15 09:10:44 -0400

    use local login info to access db

--- bloggies_backend/src/db.ts
+++ bloggies_backend/src/db.ts
@@ -1,19 +1,31 @@
 /** Database setup */
 
-import { Client } from "pg";
+import { Pool } from "pg";
 
-let DB_URI;
+// let DB_URI;
 
-if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///learning_circle_test";
-} else {
-  DB_URI = "postgresql:///learning_circle";
-}
+// if (process.env.NODE_ENV === "test") {
+//   DB_URI = "postgresql:///learning_circle_test";
+// } else {
+//   DB_URI = "postgresql:///learning_circle";
+// }
 
-let db = new Client({
-  connectionString: DB_URI
+let db = new Pool({
+  user: "postgres",
+  password: "mypassword123",
+  host: "localhost",
+  port: 5432,
+  database: "learning_circle"
+  //connectionString: DB_URI
 });
 
+
+
 db.connect();
+
+// db.query("SELECT * FROM posts", (err, res) => {
+//   console.log(err ? err.stack : res.rows); 
+//   db.end();
+// });
 
 export default db;
