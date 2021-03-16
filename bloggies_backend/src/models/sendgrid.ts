c37a51b2f50554d5e5ac4ffc432b94389dccb7ed
Megan McDevitt <mmcdevitt@blend.com>
2021-03-16 12:53:26 -0400

    Add email options to SendGrid

--- bloggies_backend/src/models/sendgrid.ts
+++ bloggies_backend/src/models/sendgrid.ts
@@ -1,4 +1,5 @@
 import sgMail from '@sendgrid/mail';
+import { MailDataRequired } from '@sendgrid/helpers/classes/mail';
 import ExpressError from "../expressError";
 import { SENDGRID_API_KEY } from '../config';
 
@@ -9,13 +10,32 @@ if(SENDGRID_API_KEY) {
 }
 
 export default class Email {
-    static async sendConfirmation(sendTo: string): Promise<void> {
-        const msg = {
+    static async sendConfirmation(sendTo: string, type: string): Promise<void> {
+        let subject;
+        let text;
+        let html;
+        switch (type) {
+            case 'confirmed':
+                subject = 'Confirmation Email from Learning Circle';
+                text = 'Welcome to the Learning Circle!';
+                break;
+            case 'pending':
+                subject = 'We need more information';
+                text = 'Before we can confirm your membership, please follow the link to answer questions';
+                html = '<a href="/">Click here to answer questions</a>'
+                break;
+            case 'rejected':
+                subject = 'Regrets from Learning Circle';
+                text = 'Thank you for your interest, but unfortunately we cannot grant you membership at this time.'
+        }
+        console.log(subject)
+        const msg:MailDataRequired =  {
             to: sendTo, // recipient
             from: 'mmcdevitt@blend.com', // verified sender
-            subject: 'Confirmation Email from Learning Circle',
-            text: 'Welcome to the Learning Circle!',
-            //html: '<strong>and easy to do anywhere, even with Node.js</strong>' //html possible - example from SendGrid
+            subject,
+            text,
+            html,
+            templateId: '13b8f94f-bcae-4ec6-b752-70d6cb59f932' //temporary template!
         }
 
         try {
--- bloggies_backend/src/routes/sendgrid.ts
+++ bloggies_backend/src/routes/sendgrid.ts
@@ -6,8 +6,8 @@ export const sendgridRouter = express.Router();
 /** POST  sends generic email confirmation*/
 sendgridRouter.post("/send-confirmation", async function (req: Request, res: Response, next: NextFunction) {
   try {
-    const { sendTo }= req.body;
-    await Email.sendConfirmation(sendTo);
+    const { sendTo, type } = req.body;
+    await Email.sendConfirmation(sendTo, type);
     res.status(200).json(`Confirmation email sent to ${sendTo}`);
   } catch (err) {
     return next(err);
