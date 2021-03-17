e5df21a2aca703efe4daddd240e316e3cf19fc71
Stefanos Ugbit <stefanos.ugbit@gmail.com>
2021-03-17 10:11:50 -0400

    added element to successPage

--- bloggies_frontend/src/RoutedPages/PaymentSuccessPage/index.tsx
+++ bloggies_frontend/src/RoutedPages/PaymentSuccessPage/index.tsx
@@ -4,6 +4,8 @@ function PaymentSuccessPage() {
   return (
     <div className="PaymentSuccessPage">
         Membership payment success redirect
+
+        <h3>Congrats on being a premium username</h3>
     </div>
   );
 };
