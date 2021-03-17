df6ba61f5226d87d8727b94ee7265f909801a4b6
abquansah <aquansah@blend.com>
2021-03-17 14:55:18 -0700

    added UserQuestions component for template of questions on form

---
+++ bloggies_frontend/src/RoutedPages/UserApplicationForm/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function UserApplicationForm() {
+  return (
+    <div className="UserApplicationForm">
+      <form>
+        
+      </form>
+    </div>
+  );
+};
+
+export default UserApplicationForm;
---
+++ bloggies_frontend/src/UserQuestions/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserQuestions = () => {
+  return (
+    <div className="UserQuestions">
+      <h3>
+        Questions Displayed Here!
+      </h3>
+    </div>
+  );
+};
+
+export default UserQuestions;
