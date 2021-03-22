fe649375cd5a40f659cccc984efdf29126777200
abquansah <aquansah@blend.com>
2021-03-22 11:29:55 -0700

    made changes to question format

--- bloggies_frontend/src/UserQuestions/index.tsx
+++ bloggies_frontend/src/UserQuestions/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { Form } from "react-bootstrap";
 
 interface IProps {
@@ -14,10 +14,11 @@ interface IProps {
 }
 const UserQuestions = ({ question, questionNumber,setAnswers, answers }: IProps) => {
 
+
   const submitAnswersHandler = (e?: any) => {
-      let answersCopy = {...answers};
-      answersCopy[questionNumber] = e.target.value;
-      setAnswers(answersCopy);
+    let answersCopy = {...answers};
+    answersCopy[questionNumber] = e.target.value;
+    setAnswers(answersCopy);
   }
 
   return (
