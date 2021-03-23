d185c4bd8d1a0684008e392cfc456b52e8a8da92
Diana <dliang@blend.com>
2021-03-23 10:02:14 -0700

    Add cases to webhook endpoint. Fix Routing order in frontend

--- bloggies_backend/src/routes/stripe.ts
+++ bloggies_backend/src/routes/stripe.ts
@@ -12,6 +12,38 @@ export const stripe = new Stripe(TEST_STRIPE_API_KEY as string, {
   apiVersion: "2020-08-27",
 });
 
+/** GET handles events that occur over a stripe session via webhook */
+stripeRouter.post("/webhook", async function (req: Request, res: Response, next: NextFunction) {
+  let event = req.body;
+
+  switch (event.type) {
+    case 'invoice.paid':
+      const invoice = event.data.object;
+      console.log(
+        `invoice paid for: ${invoice.customer}, 
+          email: ${invoice.customer_email}. 
+          subscription: ${invoice.subscription}`);
+      break;
+    case 'invoice.payment_failed':
+      console.log("invoice failed");
+      break;
+    case 'customer.subscription.deleted':
+      console.log("subscription deleted");
+      break;
+    case 'payment_intent.succeeded':
+      console.log(`PaymentIntent success for ${event.data.object.amount}`);
+      break;
+    case 'payment_method.attached':
+      console.log("attached");
+      break;
+    default:
+      console.log("web hook default, unhandled event", event.type);
+      break;
+  }
+
+  return res.json({ received: true });
+});
+
 /** POST /create-checkout-session - creates a new checkout session.
  * Returns session id, currently not checking if user logged in */
 stripeRouter.post("/create-checkout-session", async function (req: Request, res: Response, next: NextFunction) {
@@ -24,32 +56,6 @@ stripeRouter.post("/create-checkout-session", async function (req: Request, res:
   }
 });
 
-stripeRouter.get("/webhook", async function (req: Request, res: Response, next: NextFunction) {
-  let event = req.body;
-
-  switch (event.type) {
-
-    case 'invoice.paid':
-      console.log("invoice paid");
-      break;
-    case 'invoice.payment_failed':
-      console.log("invoice failed");
-      break;
-    case 'customer.subscription.deleted':
-      console.log("subscription deleted");
-      break;
-    case 'payment_intent.succeeded':
-      console.log("success");
-      break;
-    case 'payment_method.attached':
-      console.log("attached");
-      break;
-    default:
-      console.log("web hook default, unhandled event", event.type);
-  }
-
-  return res.json({ received: true });
-});
 
 /** POST create a customer for a user */
 stripeRouter.post("/create-customer", ensureLoggedIn, async function (req: Request, res: Response, next: NextFunction) {
