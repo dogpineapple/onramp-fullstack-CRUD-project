f61d98d50dc23acd061150d2a1ea172fb0e732dd
abquansah <aquansah@blend.com>
2021-03-24 23:36:59 -0700

    added dispatch method to CancelModal for subscription cancellation

--- bloggies_frontend/src/CancelModal/index.tsx
+++ bloggies_frontend/src/CancelModal/index.tsx
@@ -1,15 +1,28 @@
 import React, { Fragment, useState } from "react";
 import { Modal, Button } from "react-bootstrap";
 import styled from "styled-components";
+import { cancelPremiumUserMembership } from "../redux/actionCreators";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { CustomReduxState } from "../custom";
 
 const ButtonWrapper = styled.div`
   margin-top: 50px;
 `;
 function CancelModal() {
+  const subscriptionId = useSelector(
+    (st: CustomReduxState) => st.user.subscription_id
+  );
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
+  const handleCancel = () => {
+    dispatch(cancelPremiumUserMembership(subscriptionId));
+    history.push("/register/membership-status");
+  };
   return (
     <Fragment>
       <ButtonWrapper>
@@ -23,7 +36,9 @@ function CancelModal() {
           <Button variant="primary" onClick={handleClose}>
             No!
           </Button>
-          <Button variant="danger">Yes!</Button>
+          <Button onClick={handleCancel} variant="danger">
+            Yes!
+          </Button>
         </Modal.Footer>
       </Modal>
     </Fragment>
