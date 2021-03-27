3dac2f2ac5666c1d3a63bc724ea05847d8d338f8
Stefanos <stefanosu422@gmail.com>
2021-03-26 20:28:30 -0400

    Update stripeAction.tsx

--- bloggies_frontend/src/redux/stripeAction.tsx
+++ bloggies_frontend/src/redux/stripeAction.tsx
@@ -9,9 +9,8 @@ import {gotServerErr, deleteServerErr } from './actionCreators'
  * action to update redux store.
  */
 
- 
 export const createCustomer = () => {
-    return async function (dispatch:Dispatch<Action>) {
+   return async function (dispatch:Dispatch<Action>) {
       const res = await fetch(`${BASE_URL}/checkout/create-customer`, {
         method: 'POST',
         credentials: 'include',
@@ -19,43 +18,21 @@ export const createCustomer = () => {
           'Content-Type': 'application/json',
         } 
       })
-      const resData = await res.json() 
+      const resData = await res.json(); 
       if(res.status == 201) {
-        dispatch(deleteServerErr())
-        dispatch(gotCustomer(resData))
+        dispatch(deleteServerErr());
+        dispatch(gotCustomer(resData));
       } else {
-        dispatch(gotServerErr(resData.error.message))
+        dispatch(gotServerErr(resData.error.message));
       }
     }
   }
  
   const gotCustomer = (customer:any) => {
-    return {type: UPDATE_CUSTOMER_ID, payload: customer} 
-      
+    return {type: UPDATE_CUSTOMER_ID, payload: customer}; 
   }
-
 
   export const gotSubscription = (subscription:any) => {
-    return { type: UPDATE_SUBSCRIPTION_ID, payload: subscription }
+    return { type: UPDATE_SUBSCRIPTION_ID, payload: subscription};
   }
-
-
-
-
-//   export const stripeTokenHandler = (token: { id: any; }) => {
-//     return async function (dispatch:Dispatch<Action>) {
-//     const paymentData = {token: token.id};  
-//     // Use fetch to send the token ID and any other payment data to your backend server.
-//     const response = await fetch(`${BASE_URL}/charge`, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify(paymentData),
-//     });
-  
-//     // Return and display the result of the charge.
-//     return response.json();
-//   }
-// }
 
